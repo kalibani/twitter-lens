84834c0500c5832a1c08349ec19d91249166d24a
kalibani <kalibani.ka@gmail.com>
2023-07-22 21:34:57 +0700

    comment button

--- src/components/SignInButton.tsx
+++ src/components/SignInButton.tsx
@@ -1,27 +1,27 @@
-import { Mumbai } from "@thirdweb-dev/chains";
-import {
-  ConnectWallet,
-  useAddress,
-  useChain,
-  useNetworkMismatch,
-} from "@thirdweb-dev/react";
-import React from "react";
+// import { Mumbai } from "@thirdweb-dev/chains";
+// import {
+//   ConnectWallet,
+//   useAddress,
+//   useChain,
+//   useNetworkMismatch,
+// } from "@thirdweb-dev/react";
+// import React from "react";
 
-type Props = {};
+// type Props = {};
 
-export default function SignInButton({}: Props) {
-  const address = useAddress();
-  const isOnWrongNetwork = useNetworkMismatch();
-  const [, switchNetwork] = useChain();
+// export default function SignInButton({}: Props) {
+//   const address = useAddress();
+//   const isOnWrongNetwork = useNetworkMismatch();
+//   const [, switchNetwork] = useChain();
 
-  if (!address) {
-    return <ConnectWallet />;
-  }
+//   if (!address) {
+//     return <ConnectWallet />;
+//   }
 
-  if (isOnWrongNetwork) {
-    return (
-      <button onClick={() => switchNetwork(Mumbai)}>Switch Network</button>
-    );
-  }
-  return <div>SignInButton</div>;
-}
+//   if (isOnWrongNetwork) {
+//     return (
+//       <button onClick={() => switchNetwork(Mumbai)}>Switch Network</button>
+//     );
+//   }
+//   return <div>SignInButton</div>;
+// }
